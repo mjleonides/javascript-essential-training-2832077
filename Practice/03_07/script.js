72d80db85355d2a9854f41134ea7bfd14d77415d
mjleonides <mj@leonides.dev>
2022-08-06 14:41:40 -0500

    Completed 03_07 practice

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,48 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+const dog1 = {
+  name: "Olive",
+  color: "golden",
+  legsNum: 4,
+  earsNum: 2,
+  favoritePerson: {
+    name: "Kenzie",
+  },
+};
+const dog2 = {
+  name: "Sage",
+  color: "white",
+  legsNum: 4,
+  earsNum: 2,
+  favoritePerson: {
+    name: "Taylor",
+  },
+};
+const dog3 = {
+  name: "Tucker",
+  color: "black",
+  legsNum: 4,
+  earsNum: 2,
+  favoritePerson: {
+    name: "Courtney",
+  },
+};
+
+let dogs = [dog1, dog2, dog3];
+let i = 0;
+
+function dogPersons() {
+  let currentDog = dogs[i];
+
+  console.log(
+    currentDog.name,
+    "'s favorite person is",
+    currentDog.favoritePerson.name
+  );
+
+  i += 1;
+}
+
+dogs.forEach(dogPersons);
