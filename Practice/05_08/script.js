417332682b091b034cbe8fd4417408768b44e574
mjleonides <mj@leonides.dev>
2022-08-22 17:29:22 -0500

    Complated practice 08_06

--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,33 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+
+const navbar = `
+<ul class="nav-list">
+  <li><a href="#">Link</a></li>
+  <li><a href="#">Link</a></li>
+  <li><a href="#">Link</a></li>
+  <li><a href="#">Link</a></li>
+  <li><a href="#">Link</a></li>
+</ul>
+`;
+
+const header = document.querySelector("header");
+
+const newNav = document.createElement("nav");
+newNav.classList.add("navbar");
+newNav.setAttribute("id", "navbar");
+newNav.innerHTML = navbar;
+
+header.append(newNav);
+
+const deskItems = ["mouse", "notebook", "water", "ring", "keyboard"];
+
+deskItems.forEach(function (item) {
+  if (item === "mouse") {
+    console.log(item, "is outta here!");
+    deskItems.splice(deskItems.indexOf(item), 1);
+  }
+});
+
+console.log(deskItems);
--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -5,3 +5,15 @@
  * - Main function creates article element with data from object
  * - Helper function creates.
  */
+
+function classGunDec() {
+  document.querySelector("main").classList.toggle("active");
+}
+
+const classGunExp = function () {
+  document.querySelector("main").classList.toggle("active");
+};
+
+const classGunArrow = () => {
+  document.querySelector("main").classList.toggle("active");
+};
